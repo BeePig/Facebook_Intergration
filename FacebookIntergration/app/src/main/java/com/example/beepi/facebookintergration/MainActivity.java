93b7e6f304a88226836c0397db990723096c7cf5
ongnau <beepig025@gmail.com>
2017-01-09 16:11:10 +0700

    share bang intent

---
+++ FacebookIntergration/app/src/main/java/com/example/beepi/facebookintergration/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.beepi.facebookintergration;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.imageView1);
+    }
+
+    public void open(View view) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Hello, from tutorialspoint");
+        startActivity(Intent.createChooser(shareIntent, "Share your thoughts"));
+
+    }
+
+}
